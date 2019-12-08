d2dea67961f830bfea45a79bcabb2d029285f489
Matej Postolka <xposto02@stud.fit.vutbr.cz>
2019-12-08 21:37:45 +0100

    Make arguments of getStartAndEndNodeIndexes() const references

--- gal.cpp
+++ gal.cpp
@@ -15,12 +15,14 @@ struct Edge {
 
 typedef adjacency_list<vecS, vecS, directedS, Vertex, Edge> graph_t;
 
-graph_t getGraph(std::string filename);
-bool getStartAndEndNodeIndexes(graph_t graph, std::string startNode, std::string endNode, int &startNodeIndex, int &endNodeIndex);
+graph_t getGraph(const std::string &filename);
+bool getStartAndEndNodeIndexes(const graph_t &graph, const std::string &startNode, const std::string &endNode, int &startNodeIndex, int &endNodeIndex);
 
 int main(int argc, char *argv[]) {
     std::string filename, startNode, endNode;
-    int startNodeIndex, endNodeIndex;
+    int startNodeIndex{};
+    int endNodeIndex{};
+
     if(argc != 4)
     {
         std::cerr << "Wrong number of arguments" << std::endl;
@@ -43,7 +45,7 @@ int main(int argc, char *argv[]) {
 
 }
 
-graph_t getGraph(std::string filename)
+graph_t getGraph(const std::string &filename)
 {
     // Construct an empty graph and prepare the dynamic_property_maps.
     graph_t graph(0);
@@ -65,12 +67,12 @@ graph_t getGraph(std::string filename)
     return graph;
 }
 
-bool getStartAndEndNodeIndexes(graph_t graph, std::string startNode, std::string endNode, int &startNodeIndex, int &endNodeIndex)
+bool getStartAndEndNodeIndexes(const graph_t &graph, const std::string &startNode, const std::string &endNode, int &startNodeIndex, int &endNodeIndex)
 {
     int index = 0;
     bool startNodeFound, endNodeFound;
     startNodeFound = endNodeFound = false;
-    for(auto vertex : graph.m_vertices)
+    for(const auto &vertex : graph.m_vertices)
     {
         if(vertex.m_property.name == startNode)
         {
