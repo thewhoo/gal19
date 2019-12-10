2ccaa6e5c1bb394eb93e54175e50a5e5cdd5c556
Matej Postolka <matej@postolka.net>
2019-12-10 20:51:21 +0100

    Add basic example graphs, graph generator and reference output

---
+++ graphs/dot/gen.py
@@ -0,0 +1,55 @@
+from string import ascii_lowercase
+
+
+def dense_static_wt():
+    with open("g3_dense_w1.dot", "w") as f:
+        f.write("digraph {\n")
+        for c in ascii_lowercase:
+            f.write(f"{c};\n")
+        for c in ascii_lowercase:
+            for d in ascii_lowercase:
+                f.write(f"{c} -> {d} [weight=1];\n")
+
+        f.write("}\n")
+
+
+def dense_nosl_static_wt():
+    with open("g3_dense_w1_nosl.dot", "w") as f:
+        f.write("digraph {\n")
+        for c in ascii_lowercase:
+            f.write(f"{c};\n")
+        for c in ascii_lowercase:
+            for d in ascii_lowercase:
+                if c != d:
+                    f.write(f"{c} -> {d} [weight=1];\n")
+
+        f.write("}\n")
+
+
+def dense_dyn_wt():
+    with open("g3_dense_wd.dot", "w") as f:
+        f.write("digraph {\n")
+        for c in ascii_lowercase:
+            f.write(f"{c};\n")
+        wt = 1
+        for c in ascii_lowercase:
+            for d in ascii_lowercase:
+                f.write(f"{c} -> {d} [weight={wt}];\n")
+                wt += 1
+
+        f.write("}\n")
+
+
+def dense_nosl_dyn_wt():
+    with open("g3_dense_wd_nosl.dot", "w") as f:
+        f.write("digraph {\n")
+        for c in ascii_lowercase:
+            f.write(f"{c};\n")
+        wt = 1
+        for c in ascii_lowercase:
+            for d in ascii_lowercase:
+                if c != d:
+                    f.write(f"{c} -> {d} [weight={wt}];\n")
+                    wt += 1
+
+        f.write("}\n")
